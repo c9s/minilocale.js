2fb18201596d6c8c0ed2f8a6673f3b330763f3ad
c9s <cornelius.howl@gmail.com>
2011-05-22 18:15:46 +0800

    Inject loc method to caller's prototype

--- minilocale.js
+++ minilocale.js
@@ -5,8 +5,21 @@ MiniLocale.js
 
 */
 var MiniLocale = function(dict,lang) {
+
+    // console.log( arguments.callee.caller );
+    var caller = arguments.callee.caller;
+    caller.prototype.loc = this.loc; // inject loc method.
+
     this.lang = lang;
     this.dict = dict || { };
+
+    var that = this;
+    caller.prototype.loc = function(msg) {
+        if( that.dict[ that.lang ][ msg ] )
+            return that.dict[ that.lang ][ msg ];
+        return msg;
+    };
+
 };
 MiniLocale.prototype = {
     switch: function(lang) {
@@ -15,10 +28,5 @@ MiniLocale.prototype = {
             if( window.console )
                 console.info( lang + " dictionary is empty." );
         }
-    },
-    loc: function(msg) { 
-        if( this.dict[ this.lang ][ msg ] )
-            return this.dict[ this.lang ][ msg ];
-        return msg;
     }
 };
