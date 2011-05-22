de262aad3a7ee676019c1c82767cccf4ee3937a3
c9s <cornelius.howl@gmail.com>
2011-05-22 18:26:11 +0800

    More options

--- minilocale.js
+++ minilocale.js
@@ -4,20 +4,35 @@ MiniLocale.js
     Author: Cornelius <cornelius.howl@gmail.com>
 
 */
-var MiniLocale = function(dict,lang) {
+var MiniLocale = function(dict,lang,opts) {
+    this.opts = $.extend( {
+        ignoreCase: true,
+        ignoreLangCase: true  
+    } , opts );
 
     // console.log( arguments.callee.caller );
     var caller = arguments.callee.caller;
-    caller.prototype.loc = this.loc; // inject loc method.
+    caller.prototype.loc = this.loc; // injects loc method to caller's prototype.
+
+    if( opts.ignoreLangCase )
+        lang = lang.toLowerCase();
 
     this.lang = lang;
     this.dict = dict || { };
 
+    if( opts.ignoreCase ) {
+        // convert dictionary keys to lowercase.
+        for( var msgid in this.dict )
+            this.dict[ msgid.toLowerCase() ] = msgid;
+    }
+
     var that = this;
-    caller.prototype.loc = function(msg) {
-        if( that.dict[ that.lang ][ msg ] )
-            return that.dict[ that.lang ][ msg ];
-        return msg;
+    caller.prototype.loc = function(msgid) {
+        if( that.opts.ignoreCase )
+            return that.dict[ that.lang ][ msgid.toLowerCase() ];
+        if( that.dict[ that.lang ][ msgid ] )
+            return that.dict[ that.lang ][ msgid ];
+        return msgid;
     };
 
 };
