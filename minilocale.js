ea6b6d853a6b9c9b4ab1536ed90f076fec07fb4c
c9s <cornelius.howl@gmail.com>
2011-05-22 18:07:52 +0800

    Minor fixes

--- minilocale.js
+++ minilocale.js
@@ -1,35 +1,12 @@
 /* 
-
 MiniLocale.js
 
-Todos:
-
-    * support parsing js via ajax, and generate dictinoary json content.
-    * support json dictionary from ajax.
-
-Usage:
-
-First initialize minilocale object with dictionary 
-    and language name in your object constructor.
-
-    var Action;
-    Action = fucntion() {
-        this.locale = new MiniLocale( Action.dict , "fr" );
-        this.loc   = this.locale.loc;
-    };
-    Action.dict = {
-        zh_tw: { 
-            "Hello": "你好"
-        },
-        fr: { 
-            "Hello": "Salut"
-        }
-    };
+    Author: Cornelius <cornelius.howl@gmail.com>
 
 */
 var MiniLocale = function(dict,lang) {
     this.lang = lang;
-    this.dict = dict;
+    this.dict = dict || { };
 };
 MiniLocale.prototype = {
     switch: function(lang) {
@@ -40,7 +17,7 @@ MiniLocale.prototype = {
         }
     },
     loc: function(msg) { 
-        if( this.dict[ this.lang ] )
+        if( this.dict[ this.lang ][ msg ] )
             return this.dict[ this.lang ][ msg ];
         return msg;
     }
