ff61594c8bfd4381e47f8b451157ee96e17371fd
c9s <cornelius.howl@gmail.com>
2011-05-22 18:55:11 +0800

    Add returnLoc option

--- minilocale.js
+++ minilocale.js
@@ -6,37 +6,53 @@ MiniLocale.js
 */
 var MiniLocale = function(dict,lang,opts) {
     this.opts = $.extend( {
-        ignoreCase: true,
-        ignoreLangCase: true  
-    } , opts );
+        returnLoc: false,
+        ignoreCase: false,
+        ignoreLangCase: false,
+        injectLoc: false
+            } , opts );
 
-    // console.log( arguments.callee.caller );
-    var caller = arguments.callee.caller;
-    caller.prototype.loc = this.loc; // injects loc method to caller's prototype.
 
-    if( opts.ignoreLangCase )
+
+    if( this.opts.ignoreLangCase )
         lang = lang.toLowerCase();
 
     this.lang = lang;
     this.dict = dict || { };
 
-    if( opts.ignoreCase ) {
+    if( this.opts.ignoreCase ) {
         // convert dictionary keys to lowercase.
         for( var msgid in this.dict )
             this.dict[ msgid.toLowerCase() ] = msgid;
     }
 
     var that = this;
-    caller.prototype.loc = function(msgid) {
+    var loc = function(msgid) {
         if( that.opts.ignoreCase )
             return that.dict[ that.lang ][ msgid.toLowerCase() ];
-        if( that.dict[ that.lang ][ msgid ] )
+        if( that.dict[ that.lang ] )
             return that.dict[ that.lang ][ msgid ];
         return msgid;
     };
 
+    var caller = arguments.callee.caller;
+
+    // console.log( typeof caller );
+    if( typeof caller == "function" && this.opts.injectLoc )
+        caller.prototype.loc = loc; // injects loc method to caller's prototype.
+
+    this.loc = loc;
+
+    if( this.opts.returnLoc )
+        return loc;
 };
+
 MiniLocale.prototype = {
+
+    loc: function(msgid) {
+        return this.loc( msgid );
+    },
+
     switch: function(lang) {
         this.lang = lang;
         if( ! this.dict[ lang ] ) {
